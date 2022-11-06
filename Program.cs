236f2111b521ddc9a93eae313cca460e05f7e8b6
Ivan Chevin <chevin.gm@gmail.com>
2022-11-06 13:48:41 +0300

    решение Задачи

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string[] array = new string[4] { "hello", "2", "world", ":=)" };
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+
+string[] array2 = new string[array.Length];
+for (int i = 0, j = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+        array2[j] = array[i]; j++;
+
+}
+
+for (int i = 0; i < array2.Length; i++)
+{
+    Console.Write($"{array2[i]} ");
+}
